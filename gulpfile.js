049721d536e00e59a5e4f5c01100791c1f5c8068
William Weiss <willweiss@maxwellhealth.com>
2015-08-09 16:55:49 -0400

    docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@
  * @author Will Weiss <william.t.weiss@gmail.com>
  */
 
+"use strict";
+var fs = require("fs");
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var gulpJsdoc2md = require("gulp-jsdoc-to-markdown");
+var concat = require("gulp-concat");
+
+
 var browserify = require('browserify'),
   gulp = require('gulp'),
   transform = require('vinyl-transform'),
@@ -74,6 +82,17 @@ gulp.task('doc', function() {
         "outputSourceFiles": true
       },
     }))
+});
+
+
+gulp.task("docs", function() {
+    return gulp.src("lib/**/*.js")
+        .pipe(concat("all.md"))
+        .pipe(gulpJsdoc2md())
+        .on("error", function(err){
+            gutil.log("jsdoc2md failed:", err.message);
+        })
+        .pipe(gulp.dest("api"));
 });
 
 gulp.task('default', ['dist']);
